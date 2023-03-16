4c256bd20d577aa232d5e6194e7261784c5fd994
JosueButron <82522767+JosueButron@users.noreply.github.com>
2023-03-16 14:23:27 -0500

    4 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder} from '@angular/forms';
 import { CartService } from '../cart.service';
 
 
@@ -10,7 +11,19 @@ import { CartService } from '../cart.service';
 export class CartComponent {
   items = this.cartService.getItems();
 
+  checkoutForm = this.formBuilder.group({
+    name:'',
+    address:''
+  });
+
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
   ) { }
+
+  onSubmit(): void{
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submited', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
